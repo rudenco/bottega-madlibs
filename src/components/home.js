c7b02341285e13e03434e9f400a4f30e5e8722ec
Victor Rudenco <Victor.Rudenco@wu.com>
2018-07-23 18:23:38 +0300

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Header from './header';
+import Card from './card';
+import Input from './input';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        { Header('Botteg MadLibs')}    
+       <Card />
+
+      </div>
+    );
+  }
+}
+
+
+export default Home;
